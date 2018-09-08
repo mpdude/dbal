20088220561388aa8cd175c2f97ec629db959641
Matthias Pigulla <mp@webfactory.de>
2018-09-09 00:57:17 +0200

    Remove binary field from BlobTest and make sure both "text" and "blob" type fields can be used with LARGE_OBJECT bindings

--- tests/Doctrine/Tests/DBAL/Functional/BlobTest.php
+++ tests/Doctrine/Tests/DBAL/Functional/BlobTest.php
@@ -29,7 +29,6 @@ class BlobTest extends \Doctrine\Tests\DbalFunctionalTestCase
             $table->addColumn('id', 'integer');
             $table->addColumn('clobfield', 'text');
             $table->addColumn('blobfield', 'blob');
-            $table->addColumn('binaryfield', 'binary', array('length' => 50));
             $table->setPrimaryKey(array('id'));
 
             $sm = $this->_conn->getSchemaManager();
@@ -46,10 +45,8 @@ class BlobTest extends \Doctrine\Tests\DbalFunctionalTestCase
             'id'          => 1,
             'clobfield'   => 'test',
             'blobfield'   => 'test',
-            'binaryfield' => 'test',
         ], [
             ParameterType::INTEGER,
-            ParameterType::STRING,
             ParameterType::LARGE_OBJECT,
             ParameterType::LARGE_OBJECT,
         ]);
@@ -61,18 +58,16 @@ class BlobTest extends \Doctrine\Tests\DbalFunctionalTestCase
     {
         $this->_conn->insert('blob_table', [
             'id'          => 1,
-            'clobfield'   => 'ignored',
+            'clobfield'   => fopen('data://text/plain,test', 'r'),
             'blobfield'   => fopen('data://text/plain,test', 'r'),
-            'binaryfield' => fopen('data://text/plain,test', 'r'),
         ], [
             ParameterType::INTEGER,
-            ParameterType::STRING,
             ParameterType::LARGE_OBJECT,
             ParameterType::LARGE_OBJECT,
         ]);
 
+        $this->assertClobContains('test');
         $this->assertBlobContains('test');
-        $this->assertBinaryContains('test');
     }
 
     public function testInsertCanHandleStreamLongerThanChunkSize()
@@ -81,16 +76,15 @@ class BlobTest extends \Doctrine\Tests\DbalFunctionalTestCase
 
         $this->_conn->insert('blob_table', [
             'id'          => 1,
-            'clobfield'   => 'ignored',
+            'clobfield'   => fopen('data://text/plain,' . $longBlob, 'r'),
             'blobfield'   => fopen('data://text/plain,' . $longBlob, 'r'),
-            'binaryfield' => 'ignored',
         ], [
             ParameterType::INTEGER,
-            ParameterType::STRING,
             ParameterType::LARGE_OBJECT,
             ParameterType::LARGE_OBJECT,
         ]);
 
+        $this->assertClobContains($longBlob);
         $this->assertBlobContains($longBlob);
     }
 
@@ -100,14 +94,13 @@ class BlobTest extends \Doctrine\Tests\DbalFunctionalTestCase
             'id'          => 1,
             'clobfield'   => 'test',
             'blobfield'   => 'test',
-            'binaryfield' => 'test',
         ], [
             ParameterType::INTEGER,
-            ParameterType::STRING,
             ParameterType::LARGE_OBJECT,
             ParameterType::LARGE_OBJECT,
         ]);
 
+        $this->assertClobContains('test');
         $this->assertBlobContains('test');
     }
 
@@ -117,66 +110,62 @@ class BlobTest extends \Doctrine\Tests\DbalFunctionalTestCase
             'id' => 1,
             'clobfield' => 'test',
             'blobfield' => 'test',
-            'binaryfield' => 'test',
         ], [
             ParameterType::INTEGER,
-            ParameterType::STRING,
             ParameterType::LARGE_OBJECT,
             ParameterType::LARGE_OBJECT,
         ]);
 
         $this->_conn->update('blob_table', [
+            'clobfield' => 'test2',
             'blobfield' => 'test2',
-            'binaryfield' => 'test2',
         ], ['id' => 1], [
             ParameterType::LARGE_OBJECT,
             ParameterType::LARGE_OBJECT,
             ParameterType::INTEGER,
         ]);
 
+        $this->assertClobContains('test2');
         $this->assertBlobContains('test2');
-        $this->assertBinaryContains('test2');
     }
 
     public function testUpdateProcessesStream()
     {
         $this->_conn->insert('blob_table', [
             'id'          => 1,
-            'clobfield'   => 'ignored',
+            'clobfield'   => 'test',
             'blobfield'   => 'test',
-            'binaryfield' => 'test',
         ], [
             ParameterType::INTEGER,
-            ParameterType::STRING,
             ParameterType::LARGE_OBJECT,
             ParameterType::LARGE_OBJECT,
         ]);
 
         $this->_conn->update('blob_table', [
             'id'          => 1,
+            'clobfield'   => fopen('data://text/plain,test2', 'r'),
             'blobfield'   => fopen('data://text/plain,test2', 'r'),
-            'binaryfield' => fopen('data://text/plain,test2', 'r'),
         ], ['id' => 1], [
             ParameterType::INTEGER,
             ParameterType::LARGE_OBJECT,
             ParameterType::LARGE_OBJECT,
         ]);
 
+        $this->assertClobContains('test2');
         $this->assertBlobContains('test2');
-        $this->assertBinaryContains('test2');
     }
 
-    private function assertBinaryContains($text)
+    private function assertClobContains($text)
     {
         $rows = $this->_conn->fetchAll('SELECT * FROM blob_table');
 
         self::assertCount(1, $rows);
         $row = array_change_key_case($rows[0], CASE_LOWER);
 
-        $blobValue = Type::getType('binary')->convertToPHPValue($row['binaryfield'], $this->_conn->getDatabasePlatform());
+        $blobValue = Type::getType('text')->convertToPHPValue($row['clobfield'], $this->_conn->getDatabasePlatform());
 
-        self::assertInternalType('resource', $blobValue);
-        self::assertEquals($text, stream_get_contents($blobValue));
+        self::assertInternalType('string', $blobValue);
+        self::assertEquals($text, $blobValue);
     }
 
     private function assertBlobContains($text)
